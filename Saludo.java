cb8f987cb251f06ceb170ce20210a403c9edb792
JohaanGV07 <johgamvid@alu.edu.gva.es>
2024-12-14 19:20:02 +0100

    Update Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+public class Saludo{
 
+  public static void main(String[]args){
+    Scanner teclado= new Scanner (System.in);
+System.out.println("Dame tu nombre");
+    String nombre=teclado.nextLine();
+
+    System.out.println("Hola " + nombre + " espero te encuentres bien");
+
+  System.out.println("Adios " + nombre + " ha sido un placer conocerte");
+
+    teclado.close();
+
+    
+  }
+
+
+  
+}
